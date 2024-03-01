29cd8bc27113524914acec868a86310cdee2fbf5
julioclemente <julioolivlemente@gmail.com>
2024-03-01 00:21:44 -0300

    Criação de graficos para a ebac

---
+++ grafico.py
@@ -0,0 +1,20 @@
+%%writefile gasolina.csv
+dia,venda
+1,5.11
+2,4.99
+3,5.02
+4,5.21
+5,5.07
+6,5.09
+7,5.13
+8,5.12
+9,4.94
+10,5.03
+
+#importação de bibliotecas
+import seaborn as sns
+import pandas as pd
+
+df_venda= pd.read_csv('/content/gasolina.csv')
+grafico = sns.lineplot(data=df_venda, x = 'dia',y= 'venda')
+# código de geração do gráfico 
